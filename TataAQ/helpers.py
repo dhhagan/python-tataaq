14ab0f5848582b5f9d1bceea3aa497b6176d3c09
David H Hagan <david@davidhhagan.com>
2017-01-25 07:34:23 -1000

    complete rewrite and release v0.1

--- TataAQ/helpers.py
+++
@@ -1,9 +0,0 @@
-'''
-Includes helper functions
-'''
-import pandas as pd
-
-def to_dataframe(response):
-    """Turn the API response into a Pandas DataFrame
-    """
-    return pd.io.json.json_normalize(response)
--- tests/tata_aq_tests.py
+++ tests/tata_aq_tests.py
@@ -1,51 +1,83 @@
 import unittest
-import TataAQ
+import tataaq
 import pandas as pd
+import json
+from tataaq.exceptions import RequestError, KeywordError
+
+device_sn = "TREX001"
 
 class SetupTestCase(unittest.TestCase):
     def setUp(self):
         self.test_key = '2JIUSQNEJM1N8M44NALA6JA4'
-        self.api = TataAQ.TataAQ(self.test_key)
+        self.api = tataaq.TataAQ(apikey = self.test_key)
 
     def tearDown(self):
         pass
 
     def test_setup(self):
-        self.assertIsInstance(self.api, TataAQ.TataAQ)
+        self.assertIsInstance(self.api, tataaq.TataAQ)
 
-    def test_make_url(self):
-        self.assertEqual(self.api._make_url('auth/'), "https://tatacenter-airquality.mit.edu/api/v2.0/auth/")
+    def test_makebaseurl(self):
+        self.assertEqual(self.api._makebaseurl('auth/'), "https://tatacenter-airquality.mit.edu/api/v1.0/auth/")
+
+    def test_basic_get_request(self):
+        resp = self.api._get('auth/')
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.url, "https://tatacenter-airquality.mit.edu/api/v1.0/auth/")
+
+    def test_invalid_request_method(self):
+        with self.assertRaises(RequestError):
+            self.api._send('auth/', 'UPDATE')
 
     def test_ping(self):
-        status, resp = self.api.ping()
+        resp = self.api.ping()
 
-        self.assertEqual(status, 200)
+        self.assertEqual(resp.status_code, 200)
 
-    def test_ebam_data(self):
-        status, resp = self.api.ebam()
+    def test_get_devices(self):
+        resp = self.api.device()
 
-        self.assertEqual(status, 200)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.url, "https://tatacenter-airquality.mit.edu/api/v1.0/device/")
 
-    def test_ebam_with_params(self):
-        params = {
-            'expand': 1,
-            'per_page': 100
-        }
+    def test_get_device_with_keywords(self):
+        resp = self.api.device(per_page = 2)
 
-        status, resp = self.api.ebam(params = params)
+        data = resp.json()
 
-        self.assertEqual(status, 200)
-        self.assertTrue(type(resp['data'][0]) != str)
-        self.assertEqual(resp['meta']['per_page'], 100)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['meta']['per_page'], 2)
 
-    def test_ebam_data_page_number(self):
-        params = {
-            'expand': 1,
-            'per_page': 10,
-            'page': 3
-        }
+        resp = self.api.device(filter = "city,eq,Hilo")
 
-        status, resp = self.api.ebam(params = params)
+        self.assertEqual(resp.status_code, 200)
+        for each in resp.json()['data']:
+            self.assertEqual(each['city'], 'Hilo')
 
-        self.assertEqual(status, 200)
-        self.assertEqual(resp['meta']['page'], 3)
+    def test_get_ind_device(self):
+        resp = self.api.device(sn = device_sn)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.url, "https://tatacenter-airquality.mit.edu/api/v1.0/device/{}".format(device_sn))
+
+    def test_device_data(self):
+        resp = self.api.data(sn = device_sn)
+
+        self.assertEqual(resp.status_code, 200)
+
+    def test_error_with_required_kw(self):
+        with self.assertRaises(KeywordError):
+            resp = self.api.data()
+
+    def test_dataframes(self):
+        meta, resp = self.api.data(sn = device_sn, dataframe = True)
+
+        self.assertIsInstance(resp, pd.DataFrame)
+        self.assertIsNotNone(meta['page'])
+        self.assertIsNotNone(meta['pages'])
+        self.assertIsNotNone(meta['next_url'])
+        self.assertIsNotNone(meta['first_url'])
+        self.assertIsNotNone(meta['last_url'])
+        self.assertIsNotNone(meta['per_page'])
+        self.assertIsNotNone(meta['total'])
